c83249e04937328e74f57dd651704856d3f7c95f
MichaelCrenshaw <57016022+MichaelCrenshaw@users.noreply.github.com>
2021-10-17 18:58:30 -0600

    Fix recursion not ending after a solution is found, also add comments to code for readability.

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ import tkinter as tk
 import matplotlib as mpl
 import time
 
-# grid = [[0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0, 0]]
-
+# creates sudoku grid to solve
 grid = []
 grid.append([3, 0, 6, 5, 0, 8, 4, 0, 0])
 grid.append([5, 2, 0, 0, 0, 0, 0, 0, 0])
@@ -19,19 +15,7 @@ grid.append([1, 3, 0, 0, 0, 0, 2, 5, 0])
 grid.append([0, 0, 0, 0, 0, 0, 0, 7, 4])
 grid.append([0, 0, 5, 2, 0, 6, 3, 0, 0])
 
-# grid = []
-# grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
-# grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
-# grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
-# grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
-# grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
-# grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
-# grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
-# grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
-# grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
-
-
-
+# placeholder
 cell = 0
 
 def square(row, col):
@@ -58,12 +42,12 @@ def square(row, col):
             square = [grid[i][6:9] for i in range(6, 9)]
     return square
 
-
 def backtrack(cell):
-    if cell <= 81:
+    if cell <= 80:
         row = cell // 9
         col = (cell % 9)
         if grid[row][col] == 0:
+            # check all possible values for cell
             for x in range(1, 10):
                 # check for x in row
                 if x not in grid[row]:
@@ -75,44 +59,24 @@ def backtrack(cell):
                         # check if x is in square
                         if x not in square(row, col):
                             grid[row][col] = x
+                            # final crawler instance returns True
+                            if row == 8 & col == 8:
+                                return True
                             print(grid)
-                            backtrack(cell + 1)
+                            # disables crawlers once one finds a solution, creates next crawler
+                            if backtrack(cell + 1):
+                                return True
             else:
+                # removes guesses once the cell can't be solved
                 grid[row][col] = 0
                 return
         else:
-            backtrack(cell + 1)
+            # disables crawlers once one finds a solution, creates next crawler
+            if backtrack(cell + 1):
+                return True
             return
     else:
         return
-
-# def backtrack(cell):
-#     if cell <= 81:
-#         row = cell // 9
-#         col = (cell % 9)-1
-#         if grid[row][col] == 0:
-#             # try for values 1-9
-#             for x in range(1, 10):
-#                 # check for x in row
-#                 if x not in grid[row]:
-#                     # check for x in column
-#                     if x not in ((
-#                             grid[0][col], grid[1][col], grid[2][col], grid[3][col], grid[4][col], grid[5][col],
-#                             grid[6][col],
-#                             grid[7][col], grid[8][col])):
-#                         # check if x is in square
-#                         if x not in square(row, col):
-#                             puzzle[row][col] = x
-#                             print(puzzle)
-#                             backtrack(cell+1)
-#             else:
-#                 return
-#         else:
-#             backtrack(cell + 1)
-#             return
-#     else:
-#         print(puzzle)
-#         return
 
 
 def solve():
