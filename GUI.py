f275162bf89bc0ddb8d87f513c8a6a272e12b210
MichaelCrenshaw <57016022+MichaelCrenshaw@users.noreply.github.com>
2021-10-22 17:58:47 -0600

    Fix notification for puzzle creation being trapped in between TK updates.

--- GUI.py
+++ GUI.py
@@ -148,6 +148,7 @@ class Board(Frame):
     # create new puzzle for user to solve
     def __create_puzzle(self):
         self.label.configure(text="Please wait up to a minute for your puzzle...")
+        self.update()
         self.solver.make_puzzle()
         self.board_grid = self.solver.get_puzzle()
         self.start_grid = self.solver.get_puzzle()
